ab9c2d9f689d47bf678bef51842260d1d0ef2b27
Colton Woodruff <coltwood93@gmail.com>
2024-02-24 12:52:57 -0800

    refactored to process request and respond in new method

--- time_microservice.py
+++ time_microservice.py
@@ -2,25 +2,27 @@
 
 import socket
 
+def process_client(client_socket):
+    """Receives client request and sends timestamp"""
+    # format client data as integer
+    data = client_socket.recv(1024).decode("utf-8").strip()
+    print(f"Received '{data}'")
+    print(f"Sending '{data}'")
+    client_socket.sendall(data.encode("utf-8"))
+
 def main():
-    """listens for client connection and processes incoming traffic"""
-    host = '127.0.0.1'
+    """Main function to create server socket and listen for client connections"""
+    host = '127.0.0.1'  # set for running locally
     port = 13000
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
+    server_socket.bind((host, port))    # initialize socket
     server_socket.listen()
     print(f"Listening on {host}:{port}...")
     while True:
-        client_socket, addr = server_socket.accept()
+        client_socket, addr = server_socket.accept()    # accept client connection
         print(f"Connection from {addr}")
-        with client_socket:
-            while True:
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                print(f"Received {data}")
-                client_socket.sendall(data)
+        process_client(client_socket)    # process client request
 
 if __name__ == "__main__":
     main()
