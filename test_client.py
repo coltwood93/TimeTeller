6e34923f04176a564396b311225b979596fe9b87
Colton Woodruff <coltwood93@gmail.com>
2024-02-24 15:09:22 -0800

    made test client more robust

--- test_client.py
+++ test_client.py
@@ -4,11 +4,13 @@ import socket
 
 HOST = "127.0.0.1"
 PORT = 13000
-TIMEZONE = str(-8)
+count = 5
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(TIMEZONE.encode("utf-8"))
-    data = s.recv(1024)
+for i in range(count):
+    timezone = input("enter integer in range [-12 ... 14]: ")
 
-print(f"Received {data!r}")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(timezone.encode("utf-8"))
+        data = s.recv(1024)
+        print(f"Received {data!r}")
