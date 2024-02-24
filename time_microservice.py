b305ffb5f167243c311d4efb727555828e3d8d53
Colton Woodruff <coltwood93@gmail.com>
2024-02-24 12:32:04 -0800

    added loop to accept sequential connections

--- time_microservice.py
+++ time_microservice.py
@@ -3,6 +3,7 @@
 import socket
 
 def main():
+    """listens for client connection and processes incoming traffic"""
     host = '127.0.0.1'
     port = 13000
 
@@ -10,12 +11,16 @@ def main():
     server_socket.bind((host, port))
     server_socket.listen()
     print(f"Listening on {host}:{port}...")
-    
-    connection, address = server_socket.accept()
-    with connection:
-        print(f"Connection from {address}")
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                break
-            connection.sendall(data)
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Connection from {addr}")
+        with client_socket:
+            while True:
+                data = client_socket.recv(1024)
+                if not data:
+                    break
+                print(f"Received {data}")
+                client_socket.sendall(data)
+
+if __name__ == "__main__":
+    main()
